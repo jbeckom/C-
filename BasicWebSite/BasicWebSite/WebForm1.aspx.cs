02a86b98e1693dca14c9e7a6500ad67defa54505
Joshua Beckom <Joshua@BECKOM.olympus.local>
2018-01-04 10:48:09 -0500

    adding a few new files

---
+++ BasicWebSite/BasicWebSite/WebForm1.aspx.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace BasicWebSite
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            string value = TextBox1.Text;
+            Label1.Text = value;
+        }
+    }
+}
